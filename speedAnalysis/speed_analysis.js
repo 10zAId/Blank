4a93e7925480e0f6b3afd78f4e2d0f664ce370fe
10zAId <zaidahmedsheikh10@gmail.com>
2025-08-21 07:33:00 -0400

    Added Length Function

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -32,16 +32,17 @@ startTime = new Date().getTime();
                 return word !== "";
             }).length;
 
+            let wordsLength = userTypedText.length;
             var wpm = 0; // Default value
 
             if (timeElapsed !== 0 && !isNaN(typedWords)) {
                 wpm = Math.round((typedWords / timeElapsed) * 60);
             }
-
+            
             // Display the results
             var outputDiv = document.getElementById("output");
             outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
-                "<p>Words Typed: " + typedWords + "</p>" +
+            "<p>Total Length: " + wordsLength + "</p>" + "<p>Words Typed: " + typedWords + "</p>" +
                 "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
                 "<p>Words Per Minute (WPM): " + wpm + "</p>";
             }
